1ee698494abe217d3535e4c53f63f3a3f0f1304c
JOHN MARK MAGDASAL <magdasaljohnmark29@gmail.com>
2025-12-17 10:42:48 +0800

    fixing tailwind

---
+++ frontend/src/Card.jsx
@@ -0,0 +1,11 @@
+function Card (){
+    return (
+        <div className="card">
+            <img src="https://scontent.fcgy2-1.fna.fbcdn.net/v/t39.30808-1/500461201_122187849974273349_1447745106764825547_n.jpg?stp=c0.0.960.960a_dst-jpg_s200x200_tt6&_nc_cat=100&ccb=1-7&_nc_sid=e99d92&_nc_eui2=AeH25uH7XQOz11fsUNUE9gIdUMw0Tr5Jr79QzDROvkmvv7ncx9Sy9t3pl5_35rbwxPioAwpWKL1DXVmKY0YhReK7&_nc_ohc=WUEaoPyeyycQ7kNvwHwwq0Z&_nc_oc=AdnQcA98X0DRyqK_2k1jPyuECPhhvxH4BubQgWANpGbkn5MMWNL0xw_7vao6DOgmzns&_nc_zt=24&_nc_ht=scontent.fcgy2-1.fna&_nc_gid=4YbePYtCqFCiUl5J3cX2tQ&oh=00_Aflfe1YreyW-FZAMi9ynrptjrGH7i8MF4f9o9z0LK08OvA&oe=6947DB42" alt="Profile picture" />
+            <h2>John Mark</h2>
+            <p>I am currently studying in CTU.</p>
+        </div>
+    )
+}
+
+export default Card
